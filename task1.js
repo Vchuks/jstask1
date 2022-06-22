834f8e1fee8835da4900127dfdd84e9b71525419
Vchuks <64593598+Vchuks@users.noreply.github.com>
2022-06-22 18:31:51 +0100

    name print

---
+++ task1.js
@@ -0,0 +1,6 @@
+let name = "Chukwumee Onyinye";
+let height = "5ft";
+let country = "Nigeria";
+
+document.getElementById('task').innerHTML = 'My name is ' + name + ", I'm " + height + " tall and I'm from "+ country;
+console.log('My name is ' + name + ", I'm " + height + " tall and I'm from "+ country);
